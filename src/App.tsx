6d23c5a678771c8a96ade67c84dbffa4c23d775e
AlejandroZBarajas <falejandrozb@gmail.com>
2025-09-13 12:23:10 -0600

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+     
+     
+      <p className="read-the-docs">
+        DIMISA APP
+      </p>
+    </>
+  )
+}
+
+export default App
