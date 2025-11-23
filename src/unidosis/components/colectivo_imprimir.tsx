28a2ce958cc0a04ed6e3be7b62e61b844733f978
AlejandroZBarajas <falejandrozb@gmail.com>
2025-11-23 12:42:41 -0600

    imprime pdf bonitos

--- src/unidosis/components/colectivo_imprimir.tsx
+++ src/unidosis/components/colectivo_imprimir.tsx
@@ -1,75 +1,6 @@
-/* import { useState } from "react";
+import { useState } from "react";
 import type { ColectivoEntity } from "../../entities/colectivo_entity";
-
-
-interface Props {
-  colectivo: ColectivoEntity;
-}
-
-export default function ColectivoParaImprimir({ colectivo }: Props) {
-  const [open, setOpen] = useState(false);
-
-
-
-  async function handleImprimir() {
-
-  }
-
-  return (
-    <div className="border rounded-lg shadow p-4 mb-4 w-1/2  ">
-      
-      <div
-        className="flex justify-between items-center cursor-pointer"
-        onClick={() => setOpen(!open)}
-      >
-        <div>
-          <h3 className="font-bold text-lg">{colectivo.folio}</h3>
-          <p>Fecha: {colectivo.fecha}</p>
-          <p>Nombre de quien genera el colectivo: {colectivo.nombre_usuario}</p>
-        </div>
-        <button className="text-sm bg-blue-600 text-white px-3 py-1 rounded">
-          {open ? "Cerrar" : "Abrir"}
-        </button>
-      </div>
-
-      {open && (
-        <div className="mt-4">
-          <table className="w-full border">
-            <thead>
-              <tr className="bg-gray-200">
-                <th className="border p-2">Clave</th>
-                <th className="border p-2">Descripción</th>
-                <th className="border p-2">Cantidad solicitada</th>
-
-              </tr>
-            </thead>
-            <tbody>
-              {colectivo.claves.map((d) => (
-                <tr key={d.id_detalle}>
-                  <td className="border p-2">{d.clave}</td>
-                  <td className="border p-2">{d.descripcion}</td>
-                  <td className="border p-2">{d.cantidad}</td>
-                </tr>
-              ))}
-            </tbody>
-          </table>
-
-          <div className="flex justify-end mt-4">
-            <button
-              onClick={handleImprimir}
-              className="bg-green-600 text-white px-4 py-2 rounded disabled:opacity-50"
-            >
-              {"Imprimir"}
-            </button>
-          </div>
-        </div>
-      )}
-    </div>
-  );
-} */
-
-  import { useState } from "react";
-import type { ColectivoEntity } from "../../entities/colectivo_entity";
+import logo_header from '../../assets/logo_header.png'
 
 interface Props {
   colectivo: ColectivoEntity;
@@ -81,18 +12,28 @@ export default function ColectivoParaImprimir({ colectivo }: Props) {
 function handleImprimir() {
   const ventana = window.open("", "_blank");
   if (!ventana) return;
-
   ventana.document.write(`
     <html>
       <head>
         <style>
           @page {
             margin: 8mm;
+            size: auto;
+          }
+          @media print {
+            body {
+              margin: 0;
+            }
+            header, footer {
+              display: none;
+            }
           }
           body { 
             font-family: Arial, sans-serif; 
-            padding: 10px;
+            padding: 8px;
             font-size: 10px;
+            -webkit-print-color-adjust: exact;
+            print-color-adjust: exact;
           }
           table { 
             border: 1px solid #000; 
@@ -106,7 +47,13 @@ function handleImprimir() {
             text-align: left; 
             font-size: 9px;
           }
-          th { background-color: #f0f0f0; }
+          th { 
+            background-color: #f0f0f0; 
+            border: 1px solid #000;
+          }
+          tr{
+            border: 1px solid #000;
+          }
           h2 { 
             margin-bottom: 5px; 
             font-size: 14px;
@@ -115,34 +62,104 @@ function handleImprimir() {
             margin: 2px 0; 
             font-size: 10px;
           }
+          .firmas-container{
+            display: flex;
+            justify-content: space-between;
+            margin-top: 60px;
+            page-break-inside: avoid;
+          }
+          .headers-container {
+            display: flex;
+            justify-content: space-between;
+            page-break-inside: avoid;
+          }
+          .firma-box, .header-box {
+            width: 45%;
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+          }
+          .cantidades{
+            text-align: center;
+            width:50px;
+          }
+          .headers-container{
+            width: 100%;
+          }
+          .firma-linea {
+            width: 80%;
+            border-top: 1px solid #000;
+            margin-bottom: 5px;
+          }
+          .firma-texto {
+            text-align: center;
+            font-size: 9px;
+          }
+          header{
+          display:flex;
+          flex-direction: row;
+          }
+          img{
+          width:200px;
+          }
         </style>
       </head>
+      
       <body>
-        <h2>Colectivo: ${colectivo.folio}</h2>
-        <p><strong>Fecha:</strong> ${colectivo.fecha}</p>
-        <p><strong>Generado por:</strong> ${colectivo.nombre_usuario}</p>
+      <header>
+        <div class="headers-container">
+          <div class="header-box">
+            <img src=${logo_header}>
+          </div>
+          <div class="header-box">
+            <p class="firma-texto"><strong>IMSS BIENESTAR</strong></p>
+            <p class="firma-texto">HOSPITAL CHIAPAS NOS UNE</p>
+            <p class="firma-texto">DR JESÚS GILBERTO GÓMEZ MAZA</p>
+            <p class="firma-texto">COORDINACIÓN DE UNIDOSIS</p>
+            <p class="firma-texto">INSTITUTO DE SALUD</p>
+          </div>
+          <div class="header-box">
+            <h2>Colectivo: ${colectivo.folio}</h2>
+            <p><strong>Fecha de captura:</strong> ${colectivo.fecha}</p>
+            <p><strong>Cendis que solicita:</strong></p>
+            <p><strong></strong> ${colectivo.cendis}</p>
+            
+          </div>
+        </div>
+      </header>
         <table>
           <thead>
             <tr>
               <th>Clave</th>
               <th>Descripción</th>
-              <th>Cantidad</th>
+              <th class="cantidades" >Cantidad solicitada</th>
+              <th class="cantidades" >Cantidad surtida</th>
             </tr>
           </thead>
           <tbody>
-            ${colectivo.claves.map(d => `
-              <tr>
-                <td>${d.clave}</td>
-                <td>${d.descripcion}</td>
-                <td>${d.cantidad}</td>
-              </tr>
-            `).join("")}
+          ${colectivo.claves.map(d => `
+                        <tr>
+                          <td>${d.clave}</td>
+                          <td>${d.descripcion}</td>
+                          <td class="cantidades" >${d.cantidad}</td>
+                        </tr>
+                      `).join("")}
           </tbody>
         </table>
+        
+        <div class="firmas-container">
+          <div class="firma-box">
+            <div class="firma-linea"></div>
+            <p class="firma-texto"><strong>Generado por:</strong> ${colectivo.nombre_usuario}</p>
+          </div>
+          <div class="firma-box">
+            <div class="firma-linea"></div>
+            <p class="firma-texto">Nombre completo y firma de quien recibe en almacén</p>
+          </div>
+        </div>
       </body>
     </html>
   `);
-
   ventana.document.close();
   ventana.print();
 }
@@ -154,9 +171,9 @@ function handleImprimir() {
         onClick={() => setOpen(!open)}
       >
         <div>
-          <h3 className="font-bold text-lg">{colectivo.folio}</h3>
+          <h3 className="font-bold text-lg">Folio: {colectivo.folio}</h3>
           <p>Fecha: {colectivo.fecha}</p>
-          <p>Nombre de quien genera el colectivo: {colectivo.nombre_usuario}</p>
+          <p>Generado por: {colectivo.nombre_usuario}</p>
         </div>
         <button className="text-sm bg-blue-600 text-white px-3 py-1 rounded">
           {open ? "Cerrar" : "Abrir"}
@@ -178,7 +195,7 @@ function handleImprimir() {
                 <tr key={d.id_detalle}>
                   <td className="border p-2">{d.clave}</td>
                   <td className="border p-2">{d.descripcion}</td>
-                  <td className="border p-2">{d.cantidad}</td>
+                  <td className="border p-2 text-center">{d.cantidad}</td>
                 </tr>
               ))}
             </tbody>
